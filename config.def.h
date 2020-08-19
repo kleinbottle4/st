2973f2dbddd4f480746eab2d431675aa37b0189f
Syed Shah <syed@hp-1>
2020-08-19 10:42:56 +0100

    Dark pastels theme

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FreeMono:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -96,29 +96,35 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
+        "#3f3f3f",
+        "#705050",
+        "#60b48a",
+        "#dfaf8f",
+        "#9ab8d7",
+        "#dc8cc3",
+        "#8cd0d3",
+        "#dcdcdc",
+      	
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+        "#709080",
+        "#dca3a3",
+        "#72d5a3",
+        "#f0dfaf",
+        "#94bff3",
+        "#ec93d3",
+        "#93e0e3",
+        "#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
+	/*bg*/
+	"#2c2c2c",
+	/*fg*/
+	"#dcdcdc",
+	/*cursor*/
 	"#cccccc",
+	/*reverse cursor*/
 	"#555555",
 };
 
@@ -127,10 +133,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
