58387a4c0f56d2e2e1239e6d94d10f8c2c4b5078
Syed Shah <syed@hp-1>
2020-09-15 17:37:46 +0100

    foo

--- config.def.h
+++ config.def.h
@@ -99,36 +99,35 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
-        [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-        [1] = "#cc241d", /* red     */
-        [2] = "#98971a", /* green   */
-        [3] = "#d79921", /* yellow  */
-        [4] = "#458588", /* blue    */
-        [5] = "#b16286", /* magenta */
-        [6] = "#689d6a", /* cyan    */
-        [7] = "#a89984", /* white   */
-      
-        /* 8 bright colors */
-        [8]  = "#928374", /* black   */
-        [9]  = "#fb4934", /* red     */
-        [10] = "#b8bb26", /* green   */
-        [11] = "#fabd2f", /* yellow  */
-        [12] = "#83a598", /* blue    */
-        [13] = "#d3869b", /* magenta */
-        [14] = "#8ec07c", /* cyan    */
-        [15] = "#ebdbb2", /* white   */
+        [0] =  "#333333", /* hard contrast: #1d2021 / soft contrast: #32302f */
+        [1] =  "#8c4665", /* red     */
+        [2] =  "#287373", /* green   */
+        [3] =  "#7c7c99", /* yellow  */
+        [4] =  "#395573", /* blue    */
+        [5] =  "#5e468c", /* magenta */
+        [6] =  "#31658c", /* cyan    */
+        [7] =  "#899ca1", /* white   */
+                        
+        [8]  = "#3d3d3d", /* black   */
+        [9]  = "#bf4d80", /* red     */
+        [10] = "#53a6a6", /* green   */
+        [11] = "#9e9ecb", /* yellow  */
+        [12] = "#477ab3", /* blue    */
+        [13] = "#7e62b3", /* magenta */
+        [14] = "#6096bf", /* cyan    */
+        [15] = "#c0c0c0", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	/*bg*/
-	"#1f201b",
-	/*fg*/
-	"#f8f8f2",
-	/*cursor*/
-	"#f8f8f2",
+	"#202020",
+	 /*fg*/
+	"#ffffff",
+	 /*cursor*/
+	"#999993",
 	/*reverse cursor*/
-	"#555555",
+	"#66666C",
 };
 
 
